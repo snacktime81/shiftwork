03f26c3d637cc593f67bd7d99e5785a6cfd25f5e
간식time <tth8181@gmail.com>
2023-06-28 12:54:30 +0000

    Create profile

--- public/profile.js
+++ public/profile.js
@@ -6,7 +6,7 @@ async function axiosLoggedIn() {
 
 axiosLoggedIn().then( function(re){
 	const user = re.data;
-	console.log(user);
+	//console.log(user);
 	if(user[0]){
 		let div = document.querySelector('.containerProfile');
 		
@@ -19,6 +19,79 @@ axiosLoggedIn().then( function(re){
 		h2.innerHTML = user[1];
 		
 		divClassProfile.appendChild(h2);
+
+		let table = document.createElement('table');
+
+		divClassProfile.appendChild(table);
+		let thead = document.createElement('thead');
+
+		table.appendChild(thead);
+
+		let tr = document.createElement('tr');
+
+		thead.appendChild(tr);
+
+		let th = document.createElement('th');
+		th.innerHTML = '날짜';
+		th.className = 'smallFont';
+
+		thead.appendChild(th);
+
+		th = document.createElement('th');
+		th.innerHTML = '장비';
+		th.className = 'smallFont';
+
+		thead.appendChild(th);
+
+		th = document.createElement('th');
+		th.innerHTML = '시작 시간';
+		th.className = 'smallFont';
+
+		thead.appendChild(th);
+
+		th = document.createElement('th');
+		th.innerHTML = '끝 시간';
+		th.className = 'smallFont';
+
+		thead.appendChild(th);
+		
+		for( i of user[2]){
+
+
+			
+			
+			let tbody = document.createElement('tbody');
+	
+			table.appendChild(tbody);
+			
+			tr = document.createElement('tr');
+			
+			tbody.appendChild(tr);
+			
+
+			let td = document.createElement('td');
+			td.innerHTML = i.day;
+
+			tr.appendChild(td);
+
+			td = document.createElement('td');
+			td.innerHTML = i.machine;
+
+			tr.appendChild(td);
+
+			td = document.createElement('td');
+			td.innerHTML = i.starttime;
+
+			tr.appendChild(td);
+
+			td = document.createElement('td');
+			td.innerHTML = i.endtime;
+
+			tr.appendChild(td);
+				
+		}
+		
+		
 		
 	}
 	
